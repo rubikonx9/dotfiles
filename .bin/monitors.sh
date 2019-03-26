a9d414af738b4cc7eb1102abc1474c8fe34d2ca1
Tomasz Górecki <tomasz.gorecki@protonmail.com>
2019-03-26 07:52:05 +0100

    Corrected monitors script

--- .bin/monitors.sh
+++ .bin/monitors.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-echo 'export MONITOR_LAPTOP=eDP1' # Always available'
-echo 'export MONITOR_RIGHT=DP2'   # If at home'
-echo 'export MONITOR_LEFT=DP2'    # If in the office'
-echo 'export MONITOR_MIDDLE=DP3'  # If in the office'
+# Always available'
+echo 'export MONITOR_LAPTOP=eDP1'
+echo 'export MONITOR_EDID_LAPTOP=E43003FC00000000'
+
+# If at home'
+echo 'export MONITOR_RIGHT=DP2'
+echo 'export MONITOR_EDID_RIGHT=TODO'
+
+# If in the office'
+echo 'export MONITOR_LEFT=DP2'
+echo 'export MONITOR_EDID_LEFT=B31A085100005BC5'
+
+# If in the office'
+echo 'export MONITOR_MIDDLE=DP3'
+echo 'export MONITOR_EDID_MIDDLE=B31A085100036C14'
 
 echo 'export MONITORS_COUNT_STANDALONE=1'
 echo 'export MONITORS_COUNT_AT_HOME=2'
 echo 'export MONITORS_COUNT_IN_THE_OFFICE=3'
 
-echo 'export MONITORS=$(xrandr --listmonitors | pcregrep "\d+:" | awk "{ print \$NF }")'
-echo 'export MONITORS_COUNT=$(echo "${MONITORS}" | wc -l)'
+MONITORS=($(xrandr | perl -ne 'print "$1 " if $_ =~ m/ (\S+) \s+ connected/xms'))
+MONITORS_COUNT=${#MONITORS[*]}
+
+echo "export MONITORS=(${MONITORS[*]})"
+echo "export MONITORS_COUNT=($MONITORS_COUNT)"
