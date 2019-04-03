99512e5815791137968caf5bf9b6ea95c1cf4000
Tomasz Górecki <tomasz.gorecki@protonmail.com>
2019-04-03 13:21:01 +0200

    Power menu

--- .local/bin/rofi-power.sh
+++ .local/bin/rofi-power.sh
@@ -1,18 +1,47 @@
 #!/bin/bash
 
-LABEL_LOCK=" Lock"
+LABEL_CANCEL=" Cancel"
+LABEL_RESTART_POLYBAR=" Restart Polybar"
+LABEL_RESTART_I3=" Restart i3"
+LABEL_EXIT_I3=" Exit i3"
+LABEL_SETUP_MONITORS_AUTO=" Setup monitors automatically"
+LABEL_SETUP_MONITORS_MANUAL=" Setup monitors manually"
+LABEL_LOCK=" Lock screen"
 LABEL_SUSPEND=" Suspend"
 LABEL_REBOOT=" Reboot"
 LABEL_POWEROFF=" Poweroff"
 
-ACTION=$( (echo "${LABEL_LOCK}"    \
-        && echo "${LABEL_SUSPEND}" \
-        && echo "${LABEL_REBOOT}"  \
-        && echo "${LABEL_POWEROFF}") | rofi -dmenu -p "Power management" -matching fuzzy -no-custom)
+ACTION=$( (echo "${LABEL_CANCEL}"                \
+        && echo "${LABEL_RESTART_POLYBAR}"       \
+        && echo "${LABEL_RESTART_I3}"            \
+        && echo "${LABEL_EXIT_I3}"               \
+        && echo "${LABEL_SETUP_MONITORS_AUTO}"   \
+        && echo "${LABEL_SETUP_MONITORS_MANUAL}" \
+        && echo "${LABEL_LOCK}"                  \
+        && echo "${LABEL_SUSPEND}"               \
+        && echo "${LABEL_REBOOT}"                \
+        && echo "${LABEL_POWEROFF}") | rofi -dmenu -p "Power menu" -matching fuzzy -no-custom)
 
 case "${ACTION}" in
+    $LABEL_CANCEL)
+        ;;
     $LABEL_LOCK)
         xautolock -locknow
+        ;;
+    $LABEL_RESTART_I3)
+        i3-msg restart
+        ;;
+    $LABEL_EXIT_I3)
+        i3-msg exit
+        ;;
+    $LABEL_SETUP_MONITORS_AUTO)
+        setup-monitors
+        ;;
+    $LABEL_SETUP_MONITORS_MANUAL)
+        arandr
+        ;;
+    $LABEL_RESTART_POLYBAR)
+        polybar-launch
         ;;
     $LABEL_SUSPEND)
         xautolock -locknow && systemctl suspend
